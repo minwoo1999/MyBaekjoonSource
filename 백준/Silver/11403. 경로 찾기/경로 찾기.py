0c9855b4997bba34a5e8015c755c8d824f1013bf
KIM MIN WOO <79193811+minwoo1999@users.noreply.github.com>
2023-06-04 13:41:02 +0900

    [Silver I] Title: 경로 찾기, Time: 160 ms, Memory: 115532 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.py
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.py
@@ -1,29 +1,52 @@
+# # 플로이드 와샬
+# #dfs
+
+# n=int(input())
+
+# graph=[]
+
+# for i in range(n):
+#     a=list(map(int,input().split()))
+#     graph.append(a)
+    
+
+# def dfs(v):
+    
+#     for i in range(n):
+#         if graph[v][i]==1 and visited[i]==0:
+#             visited[i]=1
+#             dfs(i)
+    
+    
+
+# visited=[0 for _ in range(n)]
+# for i in range(n):
+#     dfs(i)
+#     for j in range(n):
+#         if visited[j]==1:
+#             print("1",end=" ")
+#         else:
+#             print("0",end=" ")
+#     visited=[0 for _ in range(n)]
+#     print()
+
+
 n=int(input())
+
 graph=[]
-visited=[0 for _ in range(n)]
+
 for i in range(n):
     a=list(map(int,input().split()))
     graph.append(a)
 
-
-def dfs(v):
-
-    for i in range(n):
-        if graph[v][i]==1 and visited[i]==0:
-            visited[i]=1
-            dfs(i)
-
-
 for i in range(n):
-    dfs(i)
-
     for j in range(n):
-        if visited[j]==1:
-            print("1",end=" ")
-        else:
-            print("0",end=" ")
-    print()
-    visited=[0 for _ in range(n)]
-    
+        for k in range(n):
+            if graph[j][i]==1 and graph[i][k]==1:
+                graph[j][k]=1
+                
+for i in graph:
+    for j in i:
+        print(j,end=" ")
         
-
+    print()
